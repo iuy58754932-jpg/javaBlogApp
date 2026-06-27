922cb3fc7236a5203ccab0213589653a3b945114
hisha <iuy58754932@gmail.com>
2026-06-28 00:36:59 +0900

    Add Article CRUD(get by id, update, delete)

--- src/main/java/com/example/demo/ArticleController.java
+++ src/main/java/com/example/demo/ArticleController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping("/articles")
@@ -26,5 +29,23 @@ public class ArticleController {
     @PostMapping
     public Article createArticle(@RequestBody Article article) {
         return articleService.create(article);
+    }
+
+    //1件取得
+    @GetMapping("/{id}")
+    public Article getArticle(@PathVariable Long id) {
+        return articleService.findById(id);
+    }
+
+    //更新
+    @PutMapping("/{id}")
+    public Article updateArticle(@PathVariable Long id, @RequestBody Article article) {
+        return articleService.update(id, article);
+    }
+
+    //削除
+    @DeleteMapping("/{id}")
+    public void deleteArticle(@PathVariable Long id) {
+        articleService.delete(id);
     }
 }
