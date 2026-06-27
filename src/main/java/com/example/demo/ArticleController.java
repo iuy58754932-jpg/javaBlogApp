6fd3b81802fada072f44f45f69f733eab6a4e0d9
hisha <iuy58754932@gmail.com>
2026-06-27 19:58:14 +0900

    ArticleControllerクラスを実装

---
+++ src/main/java/com/example/demo/ArticleController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/articles")
+public class ArticleController {
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    //一覧取得 : GET /articles
+    @GetMapping
+    public List<Article> getArticles() {
+        return articleService.findAll();
+    }
+
+    //登録 : POST /articles
+    public Article createArticle(@RequestBody Article article) {
+        return articleService.create(article);
+    }
+}
