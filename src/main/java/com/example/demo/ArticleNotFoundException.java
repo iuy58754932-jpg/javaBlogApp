2ee7bff98d57a5d8caaae2fa35f82853fa25d14b
hisha <iuy58754932@gmail.com>
2026-06-30 23:47:23 +0900

    commit test

--- src/main/java/com/example/demo/ArticleNotFoundException.java
+++ src/main/java/com/example/demo/ArticleNotFoundException.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ArticleNotFoundException extends RuntimeException {
     public ArticleNotFoundException(String message) {
